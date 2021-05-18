ab91b87686ade0c83c40781a78ac9aa6f7fba827
William Gomes <WilliamPg1998@gmail.com>
2021-05-18 10:16:05 -0400

    Gained access to second endpoint information

--- src/App.js
+++ src/App.js
@@ -8,57 +8,73 @@ const apiKey = 'tyjr5pv8a87cuj2zgy7az57p';
 
 const seasonUrl = 'https://api.sportradar.us/formula1/trial/v2/en/seasons.json'
 
-const url = new URL('https://proxy.hackeryou.com');
 
 function App() {
-  
-  
-  const [season, setSeason] = useState(null)
-  
-  // useEffect(() => {
-  //   url.search = new URLSearchParams({
-  //     reqUrl: summaryUrl,
-  //     "params[api_key]": apiKey
-  //   });
-  
+
+
+  const [season, setSeason] = useState("sr:stage:686252")
+
   // },[summaryUrl])
 
-  url.search = new URLSearchParams({
+  const proxySeason = new URL('https://proxy.hackeryou.com');
+  proxySeason.search = new URLSearchParams({
     reqUrl: seasonUrl,
+    "params[api_key]": apiKey
+  });
+
+
+  const summaryUrl = `https://api.sportradar.us/formula1/trial/v2/en/sport_events/${season}/summary.json`
+  console.log("This is the summary url:", summaryUrl)
+
+  
+  const proxySummary = new URL('https://proxy.hackeryou.com');
+  proxySummary.search = new URLSearchParams({
+    reqUrl: summaryUrl,
     "params[api_key]": apiKey
   });
   
   useEffect(() => {
     
-    // Grab the year2021 Variable we made when fetching the season for 2021 in the url
-    const summaryUrl = `https://api.sportradar.us/formula1/trial/v2/en/sport_events/${season}/summary.json`
-    console.log("This is the summary url:", summaryUrl)
-    
     // Fetch the url and Grab response for the seasons
-    fetch(url)
+    fetch(proxySeason)
     .then(response => response.json())
     .then(function (secondResponse) {
-
+      
       const seasons = secondResponse.stages.map((year) => {
         return {
           id: year.id,
           description: year.description
         }
-        })
-        
+      })
+
+    
+      fetch(proxySummary)
+      .then(function (summaryResponse) {
+        return summaryResponse.json()
+      })
+      .then(function (summarySecondResponse) {
+        console.log(summarySecondResponse);
+        return {
+          
+        }
+      })
+
+      console.log(proxySummary);
+      // .then(function () => {})
+      
         // console.log("This is the seasons", seasons)
         // Store the season for 2021 in a variable
         const year2021 = seasons[0].id
         setSeason(year2021)
-        
-        
+
+
       })
-      
-    }, [season])
-    
-    
-    return (
-      <Fragment>
+
+  }, [season, proxySeason, proxySummary])
+
+
+  return (
+    <Fragment>
       <h1>These are the seasons</h1>
 
       <Players />
