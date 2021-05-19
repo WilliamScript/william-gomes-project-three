a9c7b2e05e0b2a1e806635f46f4e779db2a3a070
William Gomes <WilliamPg1998@gmail.com>
2021-05-19 16:29:23 -0400

    Fixed Major API Problem

--- src/App.js
+++ src/App.js
@@ -6,23 +6,22 @@ import Players from './Players.js'
 const apiKey = 'tyjr5pv8a87cuj2zgy7az57p';
 // const url = new URL('https://api.sportradar.us/formula1/trial/v2/en/competitors');
 
-const seasonUrl = 'https://api.sportradar.us/formula1/trial/v2/en/seasons.json'
 
 
 function App() {
 
 
-  const [season, setSeason] = useState("sr:stage:686252")
+  const [season, setSeason] = useState("")
   const [racers, setRacers] = useState([])
 
-  // },[summaryUrl])
 
+  // const proxySeason = `https://proxy.hackeryou.com?reqUrl=https://api.sportradar.us/formula1/trial/v2/en/seasons.json?api_key=tyjr5pv8a87cuj2zgy7az57p`
+  const seasonUrl = 'https://api.sportradar.us/formula1/trial/v2/en/seasons.json'
   const proxySeason = new URL('https://proxy.hackeryou.com');
   proxySeason.search = new URLSearchParams({
     reqUrl: seasonUrl,
     "params[api_key]": apiKey
   });
-
 
   const summaryUrl = `https://api.sportradar.us/formula1/trial/v2/en/sport_events/${season}/summary.json`
 
@@ -31,6 +30,8 @@ function App() {
     reqUrl: summaryUrl,
     "params[api_key]": apiKey
   });
+
+  // const proxySummary = `https://proxy.hackeryou.com?reqUrl=https://api.sportradar.us/formula1/trial/v2/en/sport_events/${season}/summary.json?api_key=tyjr5pv8a87cuj2zgy7az57p`
 
   useEffect(() => {
 
@@ -46,46 +47,95 @@ function App() {
           }
         })
         const year2021 = seasons[0].id
-        // setSeason(year2021)
+        setSeason(year2021)
       })
+  }, []);
 
 
-    fetch(proxySummary)
-      .then(function (summaryResponse) {
-        return summaryResponse.json()
-      })
+  useEffect(() => {
+    if (season) {
 
-      .then(function (summarySecondResponse) {
-        const racers = summarySecondResponse.stage.competitors.map((racer) => {
-          return {
-            id: racer.id,
-            name: racer.name,
-            points: racer.result.points,
-            carNumber: racer.result.car_number,
-            position: racer.result.position,
-            teamName: racer.team.name
-          }
+      fetch(proxySummary)
+        .then(function (summaryResponse) {
+          return summaryResponse.json()
         })
-        setRacers(racers)
-        console.log(racers)
-      })
 
-    // .then(function () => {})
+        .then(function (summarySecondResponse) {
+          const localRacers = summarySecondResponse.stage.competitors.map((racer) => {
+            return {
+              id: racer.id,
+              name: racer.name,
+              points: racer.result.points,
+              carNumber: racer.result.car_number,
+              position: racer.result.position,
+              teamName: racer.team.name
+            }
+          })
+          setRacers(localRacers)
+        })
 
-    // console.log("This is the seasons", seasons)
-    // Store the season for 2021 in a variable
+    }
+  }, [season])
+  // setTimeout(() => {
+  //   fetch(proxySummary)
+  //   .then(function (summaryResponse) {
+  //     return summaryResponse.json()
+  //   })
+
+  //   .then(function (summarySecondResponse) {
+  //     const localRacers = summarySecondResponse.stage.competitors.map((racer) => {
+  //       return {
+  //         id: racer.id,
+  //         name: racer.name,
+  //         points: racer.result.points,
+  //         carNumber: racer.result.car_number,
+  //         position: racer.result.position,
+  //         teamName: racer.team.name
+  //       }
+  //     })
+  //     setRacers(localRacers)
+  //   })}, 3000)
+
+
+  // fetch(proxySummary)
+  //   .then(function (summaryResponse) {
+  //     return summaryResponse.json()
+  //   })
+
+  //   .then(function (summarySecondResponse) {
+  //     const localRacers = summarySecondResponse.stage.competitors.map((racer) => {
+  //       return {
+  //         id: racer.id,
+  //         name: racer.name,
+  //         points: racer.result.points,
+  //         carNumber: racer.result.car_number,
+  //         position: racer.result.position,
+  //         teamName: racer.team.name
+  //       }
+  //     })
+  //     setRacers(localRacers)
+  //   })
 
 
 
-  }, [season, proxySeason, proxySummary])
+
+  // .then(function () => {})
+
+  // console.log("This is the seasons", seasons)
+  // Store the season for 2021 in a variable
+
+
+
 
 
   return (
     <Fragment>
       <div className="wrapper">
-      <h1>F1 Standings</h1>
+        <h1>F1 Standings</h1>
+        {
+          racers.length ? <Players racers={racers} /> : <h2>Loading please wait..</h2>
+        }
 
-      <Players racers={racers} />
       </div>
     </Fragment>
   );
