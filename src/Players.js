a6ef3e31e95b2b013e4939db2e20b291d581a8b9
William Gomes <WilliamPg1998@gmail.com>
2021-05-20 05:59:12 -0400

    Finished Best I Can

--- src/Players.js
+++ src/Players.js
@@ -1,16 +1,19 @@
 // Seasons
 const Players = (props) => {
-    const { name, teamName, carNumber, position, points } = props
+    const { name, carNumber, position, points } = props
 
     return (
         <li>
-            <p className="position">{position === 50? 20:position}</p>
-            <div className={`${teamName} teamImg`}></div>
+            <p className="position">{position === 50 ? 20 : position}</p>
+            <div className={`${"teamName"} teamImg`}></div>
             <div className="teamAndName">
-               <p className="racerName">{name}</p> 
-               <p>{teamName}</p> 
+                <p className="racerName">{name}</p>
+                {/* <p>{teamName}</p>  */}
             </div>
-            {`${carNumber} - ${points ? points : 0}`}
+            <div className="extraInfo">
+                <p>Total Points:{points ? points : 0}</p>
+                <p>Racers Car Number:{carNumber} </p>
+            </div>
 
         </li>
     )
